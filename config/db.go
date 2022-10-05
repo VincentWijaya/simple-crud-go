44e59234b7db7358be75ab9ce707f80a7989660b
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-10-05 21:34:19 +0700

    display error when failed to connect to db

--- config/db.go
+++ config/db.go
@@ -1,15 +1,16 @@
 package config
 
 import (
+	"fmt"
 	"simple-crud/models"
 
 	"github.com/jinzhu/gorm"
 )
 
 func DBInit() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/gorm-crud?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/gorm-crud?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		panic("failed to connect to db")
+		panic(fmt.Sprintf("failed to connect to db: %+v", err))
 	}
 
 	db.AutoMigrate(models.Person{})
