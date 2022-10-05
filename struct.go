4f17168276b71e86e579962f775bcd942af93f56
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-10-05 21:11:43 +0700

    update json key for person struct

--- struct.go
+++ struct.go
@@ -4,6 +4,6 @@ import "github.com/jinzhu/gorm"
 
 type Person struct {
 	gorm.Model
-	FirstName string `json::"first_name"`
-	LastName  string `json::"last_name"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
